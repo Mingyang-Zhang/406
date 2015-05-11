af02d6772b296701ef37ce22c7830af711e0816b
MingYang <18810917760@163.com>
2015-05-11 21:55:30 +0800

    table_list

---
+++ tablelist.php
@@ -0,0 +1,14 @@
+<?php
+$con=mysql_connect("localhost","root","thebestweare") or die("401");
+if (!$con)
+ {
+  die('Could not connect: ' . mysql_error());
+ }
+mysql_select_db("smart_home",$con);
+$result = mysql_query("SHOW TABLES");  
+while($row = mysql_fetch_array($result))  
+{  
+echo $row[0]."";  
+mysql_free_result($result);  
+}
+?>
