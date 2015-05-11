5371a30bf81a19c142276623447ca2ec4b149842
MingYang <18810917760@163.com>
2015-05-11 22:21:52 +0800

    ad

--- tablelist.php
+++ tablelist.php
@@ -4,8 +4,7 @@ if (!$con)
  {
   die('Could not connect: ' . mysql_error());
  }
-mysql_select_db("smart_home",$con);
-$result = mysql_query("SHOW TABLES",$con);  
+$result = mysql_select_db("smart_home",$con);
 while($row = mysql_fetch_array($result))  
 {  
 echo $row[0]."";  
