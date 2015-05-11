2008c754317a683661375235ebaf12394f34eadd
MingYang <18810917760@163.com>
2015-05-11 15:19:02 +0800

    newdb
    Signed-off-by: MingYang <18810917760@163.com>

---
+++ newdb.php
@@ -0,0 +1,16 @@
+<?php
+header("Content-type:text/html;charset=utf");
+$con=mysql_connect("localhost","root","thebestweare") or die("401");
+if (!$con)
+ {
+  die('Could not connect: ' . mysql_error());
+ }
+mysql_select_db("smart_home",$con);
+$sql = "CREATE TABLE Pressure
+(
+pressure int
+)";
+mysql_query($sql,$con);
+mysql_close($conn);
+echo "successful";
+?>
