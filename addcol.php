fd92fd39e8f6381d92a9eb31748318f76b36947c
MingYang <18810917760@163.com>
2015-05-13 23:32:10 +0800

    by z

---
+++ addcol.php
@@ -0,0 +1,11 @@
+<?php
+$con=mysql_connect("localhost","root","thebestweare") or die("401");
+if (!$con)
+ {
+  die('Could not connect: ' . mysql_error());
+ }
+mysql_select_db("smart_home",$con);
+$sql = "ALTER TABLE Pressure ADD "Room" VARCHAR";
+mysql_query($sql) or die('402');
+mysql_close($con);
+?>
