91c77ee329a8825325eb3092356d3304391cf946
MingYang <18810917760@163.com>
2015-05-13 23:53:09 +0800

    by z

--- pressupdate.php
+++ pressupdate.php
@@ -7,7 +7,7 @@ if (!$con)
  }
 mysql_select_db("smart_home",$con);
 mysql_query("INSERT INTO Pressure (pressure) 
- VALUES (100)") or die('402');
+ VALUES (100)") or die('401');
 mysql_query("INSERT INTO Pressure (Room) 
  VALUES ('406')") or die('402');
  $result = mysql_query("SELECT * FROM Pressure");
