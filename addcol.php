90496005e2b9244588a9a9aa8bab6d47207a7004
MingYang <18810917760@163.com>
2015-05-14 00:24:24 +0800

    by z

--- addcol.php
+++ addcol.php
@@ -5,7 +5,7 @@ if (!$con)
   die('Could not connect: ' . mysql_error());
  }
 mysql_select_db("smart_home",$con);
-$sql = "ALTER TABLE 'Pressure' ADD "Room" VARCHAR(10)";
+$sql = "ALTER TABLE 'Pressure' ADD 'Room' VARCHAR(10)";
 mysql_query($sql) or die('402');
 mysql_close($con);
 ?>
