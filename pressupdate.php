54783d2eb3a754858e946bfc23c48de975d66685
MingYang <18810917760@163.com>
2015-05-13 23:49:52 +0800

    by z

--- pressupdate.php
+++ pressupdate.php
@@ -6,8 +6,8 @@ if (!$con)
   die('Could not connect: ' . mysql_error());
  }
 mysql_select_db("smart_home",$con);
-mysql_query("INSERT INTO Pressure (pressure) 
- VALUES (100)") or die('402');
+// mysql_query("INSERT INTO Pressure (pressure) 
+//  VALUES (100)") or die('402');
 // mysql_query("INSERT INTO Pressure (Room) 
 //  VALUES ('406')") or die('402');
  $result = mysql_query("SELECT * FROM Pressure");
