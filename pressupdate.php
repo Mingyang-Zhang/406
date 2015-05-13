c60e0de3c921f2a13a2861b5f0c7d6c59eb87b4d
MingYang <18810917760@163.com>
2015-05-13 23:43:14 +0800

    by z

--- pressupdate.php
+++ pressupdate.php
@@ -6,24 +6,23 @@ if (!$con)
   die('Could not connect: ' . mysql_error());
  }
 mysql_select_db("smart_home",$con);
-mysql_query("INSERT INTO Pressure (pressure) 
- VALUES (100)") or die('402');
-mysql_query("INSERT INTO Pressure (Room) 
- VALUES ('406')") or die('402');
-$result = mysql_query("SELECT * FROM Pressure");
-
-while($row = mysql_fetch_array($result))
+// mysql_query("INSERT INTO Pressure (pressure) 
+//  VALUES (100)") or die('402');
+// mysql_query("INSERT INTO Pressure (Room) 
+//  VALUES ('406')") or die('402');
+// $result = mysql_query("SELECT * FROM Pressure");
+// while($row = mysql_fetch_array($result))
+//   {
+//   echo $row['pressure'];
+//   echo "<br />";
+//   echo $row['Room'];
+//   echo "<br />";
+//   }
+ mysql_query("DELETE FROM Pressure WHERE pressure=100") or die('402');
+ while($row = mysql_fetch_array($result))
   {
   echo $row['pressure'];
   echo "<br />";
-  echo $row['Room'];
-  echo "<br />";
   }
- // mysql_query("DELETE FROM Pressure WHERE pressure=100");
- // while($row = mysql_fetch_array($result))
- //  {
- //  echo $row['pressure'];
- //  echo "<br />";
- //  }
 mysql_close($con);
 ?>
