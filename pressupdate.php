47bdacf6604ee1b9c36f3e93d674a2e9985a30a5
Mingyang-Zhang <18810917760@163.com>
2015-05-16 23:15:05 +0800

    Update pressupdate.php

--- pressupdate.php
+++ pressupdate.php
@@ -1,21 +1,23 @@
 <?php
 require_once 'login.php';
 header("Content-type:text/html;charset=utf");
-//mysql_select_db("smarthome",$con); 
-// $temperature = 0;
-// if(isset($_GET['temperature']))
-// {
-// 	$temperature = $_GET['temperature'];
-// 	echo $temperature;
-// 	$querytemp = "INSERT INTO temperature (tp) VALUES ($temperature)";
-// 	mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
-// }
 $con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 mysql_select_db("smart_home",$con);
-mysql_query("INSERT INTO Press (pressure) 
-VALUES (100)") or die('Could not connect: ' . mysql_error());
-mysql_query("INSERT INTO Press (room) 
-VALUES (406)") or die('402');
+mysql_select_db("smarthome",$con); 
+$press = 0;
+if(isset($_GET['press']))
+{
+	$press = $_GET['press'];
+	//echo $temperature;
+	$querytemp = "UPDATE Press SET pressure = $press WHERE room = 406";
+	mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
+}
+//$con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
+//mysql_select_db("smart_home",$con);
+//mysql_query("INSERT INTO Press (pressure) 
+//VALUES (100)") or die('Could not connect: ' . mysql_error());
+//mysql_query("INSERT INTO Press (room) 
+//VALUES ("406")") or die('402');
  //$result = mysql_query("SELECT * FROM Pressure");
 // while($row = mysql_fetch_array($result))
 //   {
