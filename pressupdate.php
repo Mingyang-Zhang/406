9f78b82211709c33bcfbdfbcfa252567ea5bf60e
Mingyang-Zhang <18810917760@163.com>
2015-05-16 23:02:09 +0800

    Update pressupdate.php

--- pressupdate.php
+++ pressupdate.php
@@ -1,7 +1,6 @@
 <?php
 require_once 'login.php';
 header("Content-type:text/html;charset=utf");
-$con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 //mysql_select_db("smarthome",$con); 
 // $temperature = 0;
 // if(isset($_GET['temperature']))
@@ -11,16 +10,14 @@ $con=mysql_connect("localhost","root",$db_password) or die('Could not connect: '
 // 	$querytemp = "INSERT INTO temperature (tp) VALUES ($temperature)";
 // 	mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
 // }
+$con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 if (!$con)
- {
-  die('Could not connect: ' . mysql_error());
- }
 mysql_select_db("smart_home",$con);
-// mysql_query("INSERT INTO Pressure (pressure) 
-//  VALUES (100)") or die('Could not connect: ' . mysql_error());
-// mysql_query("INSERT INTO Pressure (Room) 
-//  VALUES ("406")") or die('402');
- $result = mysql_query("SELECT * FROM Pressure");
+mysql_query("INSERT INTO Press (pressure) 
+VALUES (100)") or die('Could not connect: ' . mysql_error());
+mysql_query("INSERT INTO Press (room) 
+VALUES ("406")") or die('402');
+ //$result = mysql_query("SELECT * FROM Pressure");
 // while($row = mysql_fetch_array($result))
 //   {
 //   echo $row['pressure'];
@@ -28,11 +25,11 @@ mysql_select_db("smart_home",$con);
 //   echo $row['Room'];
 //   echo "<br />";
 //   }
- mysql_query("DELETE FROM Pressure WHERE pressure=100") or die('Could not connect: ' . mysql_error());
- while($row = mysql_fetch_array($result))
-  {
-  echo $row['pressure'];
-  echo "<br />";
-  }
+ //mysql_query("DELETE FROM Pressure WHERE pressure=100") or die('Could not connect: ' . mysql_error());
+// while($row = mysql_fetch_array($result))
+ // {
+ // echo $row['pressure'];
+ // echo "<br />";
+ //}
 mysql_close($con);
 ?>
