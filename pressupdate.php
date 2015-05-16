a3e2338032c3d29c837714aafcc7cfbcddb0541c
Mingyang-Zhang <18810917760@163.com>
2015-05-16 23:50:03 +0800

    Update pressupdate.php

--- pressupdate.php
+++ pressupdate.php
@@ -12,6 +12,7 @@ if(isset($_GET['press']))
 	echo $press;
 	$querytemp = "UPDATE Press SET pressure = $press WHERE room = '406'";
 	mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
+	mysql_query("DELETE FROM Press WHERE press='100'");
 }
 //$con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 //mysql_select_db("smart_home",$con);
