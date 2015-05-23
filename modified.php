01cbf9a15fc7e5f1320104933a3937a7b85c8d2f
MingYang <18810917760@163.com>
2015-05-23 21:55:21 +0800

    by z

--- modified.php
+++ modified.php
@@ -5,7 +5,7 @@ $con=mysql_connect("localhost","root",$db_password) or die('Could not connect: '
 mysql_select_db("smart_home",$con);
 //mysql_query("INSERT INTO Press (room) 
 //VALUES ('406')") or die('insert failed');
-$querytemp = "UPDATE Press SET pressure = 100 WHERE room = '406'";
+$querytemp = "UPDATE Press SET id=0 WHERE room = '406'";
 mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
 mysql_close($con);
 ?>
