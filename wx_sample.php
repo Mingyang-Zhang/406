f4b1cc4a30f7cefe66377788d8b48e60a29987da
Mingyang-Zhang <18810917760@163.com>
2015-05-24 03:08:49 +0800

    Create wx_sample.php

--- wx_sample.php
+++ wx_sample.php
@@ -40,12 +40,12 @@ class wechatCallbackapiTest
 
     public function responseMsg()
     {
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-        if (!empty($postStr)){
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
+        //$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        //if (!empty($postStr)){
+            //$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = "oSOIks7Vk48kGKcmR8dc7N8X9sQU";//$postObj->FromUserName;
+            $toUsername = "gh_ac29a4b32890";//$postObj->ToUserName;
+            $keyword = "hi"//trim($postObj->Content);
             $time = time();
             $textTpl = "<xml>
                         <ToUserName><![CDATA[%s]]></ToUserName>
