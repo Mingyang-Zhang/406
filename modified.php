1eb27673a8d13e9b38557d5744798a51a666be66
MingYang <18810917760@163.com>
2015-05-23 21:54:13 +0800

    by z

--- modified.php
+++ modified.php
@@ -3,9 +3,9 @@ require_once 'login.php';
 header("Content-type:text/html;charset=utf");
 $con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 mysql_select_db("smart_home",$con);
-mysql_query("INSERT INTO Press (room) 
-VALUES ('406')") or die('insert failed');
-//$querytemp = "UPDATE Press SET pressure = 100 WHERE room = '406'";
-//mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
+//mysql_query("INSERT INTO Press (room) 
+//VALUES ('406')") or die('insert failed');
+$querytemp = "UPDATE Press SET pressure = 100 WHERE room = '406'";
+mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
 mysql_close($con);
 ?>
