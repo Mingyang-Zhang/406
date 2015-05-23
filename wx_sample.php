e3c68459a8829ada48bc5c78aaeb0d04835e9497
Mingyang-Zhang <18810917760@163.com>
2015-05-24 01:25:16 +0800

    Update wx_sample.php

--- wx_sample.php
+++ wx_sample.php
@@ -29,39 +29,40 @@ class wechatCallbackapiTest
     
     public function responseMsg()
     {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-		
-		
-		$conn = mysql_connect("localhost".":"."3306","root","thebestweare");
-		mysql_select_db("smart_home",$conn);
-		$res = mysql_query("select * from Press where pressure>100");
-		while($rows = mysql_fetch_assoc($res)){      
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        if (!empty($postStr)){
             $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $conn = mysql_connect("localhost".":"."3306","root","thebestweare");
+	    mysql_select_db("smart_home",$conn);
+	    $res = mysql_query("select * from Press where pressure>100");
+	    $contentStr="";
+	    while($rows = mysql_fetch_assoc($res){
+	    	$contentStr=$contentStr." ".$rows[pressure];
+	    }
             $fromUsername = $postObj->FromUserName;
             $toUsername = $postObj->ToUserName;
-		
-			$type = $postObj->MsgType;
-                //echo $type;
-			$customevent = $postObj->Event;
-				//echo $customevent;	                
-
-                $time = time();
-                $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>"; 
-			
-		  		$contentStr = $rows[pressure];
-                  //echo $contentStr;
-		  		$msgType = "text";
-                  $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                  echo $resultStr;
-		}		
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+            if($keyword == "?" || $keyword == "？")
+            {
+                $msgType = "text";
+                $contentStr = date("Y-m-d H:i:s",time())
+            } 
+            $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            echo $resultStr;
+        }else{
+            echo "";
+            exit;
+        }
     }
 		
 	private function checkSignature()
@@ -82,6 +83,7 @@ class wechatCallbackapiTest
 			return false;
 		}
 	}
+	
 }
 
 ?>
