de538e37d9b445ce913142934f970fafa6c25300
MingYang <18810917760@163.com>
2015-05-23 20:49:30 +0800

    by z

--- pressupdate.php
+++ pressupdate.php
@@ -1,22 +1,22 @@
 <?php
 require_once 'login.php';
 header("Content-type:text/html;charset=utf");
-//$con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
-//mysql_select_db("smart_home",$con);
-//$press = 0;
-//echo $press;
-//if(isset($_GET['press']))
-//{
-//	$press = $_GET['press'];
-//	echo $press;
-//	$querytemp = "UPDATE Press SET pressure = $press WHERE room = '406'";
-//	mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
-//	mysql_query("DELETE FROM Press WHERE pressure='100'");
-//}
 $con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 mysql_select_db("smart_home",$con);
-mysql_query("INSERT INTO Press (room) 
-VALUES ('406')") or die('insert failed');
+$press = 0;
+echo $press;
+if(isset($_GET['press']))
+{
+	$press = $_GET['press'];
+	echo $press;
+	$querytemp = "UPDATE Press SET pressure = $press WHERE room = '406'";
+	mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
+	mysql_query("DELETE FROM Press WHERE pressure='100'");
+}
+//$con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
+//mysql_select_db("smart_home",$con);
+//mysql_query("INSERT INTO Press (room) 
+//VALUES ('406')") or die('insert failed');
  //$result = mysql_query("SELECT * FROM Pressure");
 // while($row = mysql_fetch_array($result))
 //   {
