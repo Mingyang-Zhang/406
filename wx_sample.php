c4508e04421d5ec8e5c8d0822cbccf5c67c7cf01
MingYang <18810917760@163.com>
2015-05-23 21:31:14 +0800

    by z

--- wx_sample.php
+++ wx_sample.php
@@ -1,5 +1,4 @@
 <?php
-require_once 'login.php';
 /**
   * wechat php test
   */
@@ -21,25 +20,27 @@ class wechatCallbackapiTest
         	exit;
         }
     }
-
+	 
+    
     public function responseMsg()
     {
 		//get post data, May be due to the different environments
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-      	//extract post data
-		if (!empty($postStr)){
-                
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
 		
-		$type = $postObj->MsgType;
+		
+		$conn = mysql_connect("localhost".":"."3306","root","thebestweare");
+		mysql_select_db("smart_home",$conn);
+		$res = mysql_query("select * from Press where pressure>100");
+		while($rows = mysql_fetch_assoc($res)){      
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+		
+			$type = $postObj->MsgType;
                 //echo $type;
-		$customevent = $postObj->Event;
-		//echo $customevent;	                
+			$customevent = $postObj->Event;
+				//echo $customevent;	                
 
-		$keyword = trim($postObj->Content);
                 $time = time();
                 $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
@@ -49,43 +50,13 @@ class wechatCallbackapiTest
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
 							</xml>"; 
-/***/			
-		if($type=="event" and $customevent=="subscribe"){
-		  $contentStr = "你发送“变”，我就变哟！！";
+			
+		  		$contentStr = $rows[pressure];
                   //echo $contentStr;
-		  $msgType = "text";
+		  		$msgType = "text";
                   $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
                   echo $resultStr;
-		}
-
-/**/            
-		else if( !empty($keyword))
-                {
-              		$msgType = "text";
-                        $contentStr = "你说“变”，我才变！！";
-                      if($keyword == "变") { 
-                	$contentStr = "看我变！";
-                	
-/**/                   
-			$conn = mysql_connect("localhost".":"."3306","root",$db_password);
-			mysql_select_db("smart_home",$conn);
-			$result = mysql_query("select * from comd");
-			$row = mysql_fetch_array($result);
-                        $next_cmd = 1-(int)$row["cmd"];
-                        $query =sprintf("update comd set cmd ='%s' where id=1",(string)$next_cmd);
-                        mysql_query($query);
-/**/
-			}
-			$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
-
-        }else {
-        	echo "";
-        	exit;
-        }
+		}		
     }
 		
 	private function checkSignature()
