a4a14dd0778d9589f09eaad68795b94eedcbd1b4
MingYang <18810917760@163.com>
2015-05-24 04:42:07 +0800

    by z

--- modified.php
+++ modified.php
@@ -3,12 +3,12 @@ require_once 'login.php';
 header("Content-type:text/html;charset=utf");
 $con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 mysql_select_db("smart_home",$con);
-mysql_query("UPDATE Press SET flag = 1 WHERE room = 406");
+//mysql_query("UPDATE Press SET flag = 1 WHERE room = 406");
 //$querytemp = "UPDATE Press SET id=0 WHERE room = '406'";
 //mysql_query($querytemp) or die('Could not connect: ' . mysql_error());
 //$con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 //mysql_select_db("smart_home",$con);
-//mysql_query("INSERT INTO Press (room,id) 
-//VALUES ('406','1')") or die('insert failed');
+mysql_query("INSERT INTO location (id) 
+//VALUES ('0')") or die('insert failed');
 mysql_close($con);
 ?>
