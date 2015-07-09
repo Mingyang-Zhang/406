a7c38e9db558bdb5290a697a6981dcc6ffea964f
Mingyang-Zhang <18810917760@163.com>
2015-07-09 20:01:28 +0800

    Update pressupdate.php

--- pressupdate.php
+++ pressupdate.php
@@ -4,6 +4,7 @@ header("Content-type:text/html;charset=utf");
 $con=mysql_connect("localhost","root",$db_password) or die('Could not connect: ' . mysql_error());
 mysql_select_db("smart_home",$con);
 $press = 0;
+echo 'land successfully';
 if(isset($_GET['press']))
 {
 	$id=$_GET['id'];
